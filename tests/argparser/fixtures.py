a9f9a045317c37b1f7ebdc7a66abcd0b4df623eb
Robin Obůrka <r.oburka@gmail.com>
2018-02-07 20:55:27 +0100

    tests: Move some fixtures to upper level and share them

--- tests/argparser/fixtures.py
+++ tests/argparser/fixtures.py
@@ -1,16 +1,6 @@
 import pytest
 
-import zmq
-
 import sn
-
-@pytest.fixture
-def zmq_context():
-    return zmq.Context.instance()
-
-@pytest.fixture
-def arg_parser():
-    return sn.get_arg_parser()
 
 @pytest.fixture
 def empty_args():
--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,2 +1,13 @@
 import pytest
 
+import zmq
+
+import sn
+
+@pytest.fixture
+def zmq_context():
+    return zmq.Context.instance()
+
+@pytest.fixture
+def arg_parser():
+    return sn.get_arg_parser()
