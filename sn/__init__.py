490ab09aab5d20a4e2d0a1bd60fbe5b0ee6387e0
Robin Obůrka <r.oburka@gmail.com>
2018-02-14 21:56:33 +0100

    Provide logging feature

--- sn/__init__.py
+++ sn/__init__.py
@@ -2,3 +2,16 @@ from sn.exceptions import *
 from sn.messages import *
 from sn.argparser import *
 from sn.network import *
+
+import logging
+import logging.handlers
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="sentinel: %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[
+        logging.FileHandler("sentinel.log", encoding="UTF-8"),
+        logging.handlers.SysLogHandler(address="/dev/log"),
+    ],
+)
