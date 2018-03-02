6e466d474ce41334bc5425073e88e325eae57dae
Robin Obůrka <robin.oburka@nic.cz>
2018-03-02 10:45:10 +0100

    argparse: Add support for verbose mode in our logger and argparser

--- sn/argparser.py
+++ sn/argparser.py
@@ -1,4 +1,11 @@
 import argparse
+import logging
+
+
+class EnableVerbose(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        root = logging.getLogger()
+        root.setLevel(logging.DEBUG)
 
 
 def get_arg_parser():
@@ -14,5 +21,10 @@ def get_arg_parser():
     parser.add_argument('--disable-ipv6',
                         action='store_true',
                         help='Disable IPv6 mode of ZMQ sockets'
+                       )
+    parser.add_argument('-v', '--verbose',
+                        nargs=0,
+                        action=EnableVerbose,
+                        help="Enables debug mode in logger"
                        )
     return parser
