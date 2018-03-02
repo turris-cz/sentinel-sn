fc8655cb89bdd00d450e3f98d7d50f22e3e3c8ac
Robin Obůrka <robin.oburka@nic.cz>
2018-03-02 10:27:44 +0100

    argparser: Rewrite argparser to more readable form

--- sn/argparser.py
+++ sn/argparser.py
@@ -5,7 +5,14 @@ def get_arg_parser():
     """ Creates own arguments parser and return it as an object.
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--resource', nargs=1, action='append', required=True,
-        help='resource format: sockname,[connect/bind],sock_type,ip_address,port')
-    parser.add_argument('--disable-ipv6', action='store_true')
+    parser.add_argument('--resource',
+                        nargs=1,
+                        action='append',
+                        required=True,
+                        help='resource format: sockname,[connect/bind],sock_type,ip_address,port'
+                       )
+    parser.add_argument('--disable-ipv6',
+                        action='store_true',
+                        help='Disable IPv6 mode of ZMQ sockets'
+                       )
     return parser
