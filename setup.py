752772b4df4c334fc18d98497ac5e8b25b192b36
Robin Obůrka <robin.oburka@nic.cz>
2018-05-10 17:10:17 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name = "sn",
-    version = "0.1",
+    version = "0.2",
     description = "Sentinel networking library",
     author = "Martin Prudek",
     author_email = "martin.prudek@nic.cz",
