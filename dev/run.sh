b10840154b9f6746eda1e15107776f513a67483e
Robin Obůrka <robin.oburka@nic.cz>
2018-11-27 10:39:26 +0100

    run: Add verbose to run script
    This is useful since the time we have monitoring with development
    backend in log files.

--- dev/run.sh
+++ dev/run.sh
@@ -23,13 +23,13 @@ tmux select-pane -t 0
 tmux send-keys "tail -f sentinel.log" C-m
 tmux select-pane -t 1
 tmux send-keys "workon sentinel" C-m
-tmux send-keys "./out_only.py --resource 'out,connect,PUSH,127.0.0.1,8801'" C-m
+tmux send-keys "./out_only.py --resource 'out,connect,PUSH,127.0.0.1,8801' -v" C-m
 tmux select-pane -t 2
 tmux send-keys "workon sentinel" C-m
-tmux send-keys "./in_only.py --resource 'in,bind,PULL,*,8802'" C-m
+tmux send-keys "./in_only.py --resource 'in,bind,PULL,*,8802' -v" C-m
 tmux select-pane -t 3
 tmux send-keys "workon sentinel" C-m
-tmux send-keys "./in_out.py --resource 'in,bind,PULL,*,8801' --resource 'out,connect,PUSH,127.0.0.1,8802'" C-m
+tmux send-keys "./in_out.py --resource 'in,bind,PULL,*,8801' --resource 'out,connect,PUSH,127.0.0.1,8802' -v" C-m
 
 # Attach the session
 tmux attach-session -t "$SESSION"
